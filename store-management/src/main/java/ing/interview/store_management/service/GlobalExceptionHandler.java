c06f4ef46cc4a376b7ed6ea1718fbf605e691537
Andreeaoprea0605 <aoprea555@gmail.com>
2024-11-10 20:18:59 +0200

    add logs

--- store-management/src/main/java/ing/interview/store_management/service/GlobalExceptionHandler.java
+++ store-management/src/main/java/ing/interview/store_management/service/GlobalExceptionHandler.java
@@ -2,26 +2,48 @@ package ing.interview.store_management.service;
 
 import ing.interview.store_management.exception.InsufficientStockException;
 import ing.interview.store_management.exception.NoValidProductInOrderException;
+import ing.interview.store_management.exception.OrderNotFoundException;
+import ing.interview.store_management.exception.ProductNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoValidProductInOrderException.class)
-    public ResponseEntity<String> handleNoValidProductInOrderException(NoValidProductInOrderException ex) {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<?> handleOrderNotFoundException(OrderNotFoundException ex) {
+        logger.error("Order not found: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<?> handleProductNotFoundException(ProductNotFoundException ex) {
+        logger.error("Product not found: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(InsufficientStockException.class)
-    public ResponseEntity<String> handleInsufficientProductQuantityException(InsufficientStockException ex) {
+    public ResponseEntity<?> handleInsufficientStockException(InsufficientStockException ex) {
+        logger.error("Insufficient stock: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NoValidProductInOrderException.class)
+    public ResponseEntity<?> handleNoValidProductInOrderException(NoValidProductInOrderException ex) {
+        logger.error("No Valid Product In Order Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
+    public ResponseEntity<?> handleGlobalException(Exception ex) {
+        logger.error("Unexpected error occurred: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected error occurred: " + ex.getMessage());
     }
 }
